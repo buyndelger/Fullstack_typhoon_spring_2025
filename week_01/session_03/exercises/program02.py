f0ce25df4f75f1fa37d96cb20296a80954c2e80d
Buyndelger <buyndelger80400727@gmail.com>
2025-03-29 18:04:00 +0800

    add: session 03

---
+++ week_01/session_03/exercises/program02.py
@@ -0,0 +1,3 @@
+MyName= input('Give me your name: ')
+MyAge = input(' Give me your age')
+print(f"Your name is{MyName}.{MyAge}yours old")
---
+++ week_01/session_03/exercises/variable.py
@@ -0,0 +1,29 @@
+# variable
+#"varable" -string, texten turul
+#1,2,3-number integer turul
+#1.5,2.5,3.5 -number float turul
+#"1.5" string turul
+#python namins convention snake_case
+
+first_name  = 'buyndelger'
+another_name ='buyndelger'
+last_name ='boldbaatar'
+age =21
+major =' BI icode acedame iin suragch'
+hobbies = 'duu sonsoh ,kino uzeh alhah '
+print(first_name)
+print(another_name)
+print(last_name)
+print(age)
+print(major)
+print(hobbies)
+is_student=True
+is_adult= False
+print(is_student)
+print(is_adult)
+
+
+print("My name is" + first_name + " " + last_name + "." + "I'm 21years old.")
+print(str(age) + "years old")
+print(f"Ner:{first_name} Obog:{last_name} Nas{age}")
+print(f"Ajil:{major} durtaizuil:{hobbies}")
