3ae24d84ecbc004c76f2b9b875361f26c6a000c7
Buyndelger <buyndelger80400727@gmail.com>
2025-04-10 17:23:18 +0800

    add: session 08

---
+++ week_03/session_08/ex01.py
@@ -0,0 +1,38 @@
+colors = ['red' ,' yellow', 'blue' , 'greaan' ,'purple']
+print(colors)
+print(colors[2])
+print(colors[0])
+print(colors[1])
+print(colors[3])
+print(colors[4])
+
+
+for a in colors:
+    print(a)
+for i in range(9):
+    print(i)
+for i in range(1,10):
+    print(i)
+for i in range(3,10,3):
+    print(i)
+
+
+while loop
+i=1
+while i <9:
+    print(i)
+    i = i+1 
+i =10
+while i <= 100:
+    print(i)
+    i=i+1
+i= 2
+while i >=20:
+    print(i)
+    i= i*2
+i=1
+while i <=20:
+    if i % 2 == 0:
+        print(i)
+    i = i+1
+
---
+++ week_03/session_08/ex02.py
@@ -0,0 +1,9 @@
+import random
+roll_dice = random.randint(1,6)
+print(roll_dice)
+is_heads =random.choice([True, False])
+print("You flippef heads : "+ str(is_heads))
+hoolniiTsugluulag = random.choice(['ondog', 'buuz', 'nogoo', 'alim'])
+print("songolt:"+str(hoolniiTsugluulag))
+too =random.choice[1,5]
+print(too)
