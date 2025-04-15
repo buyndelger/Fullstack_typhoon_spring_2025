9f2089c4364fdb6521ed657d4b8430c45ab17f4e
Buyndelger <buyndelger80400727@gmail.com>
2025-04-15 18:03:06 +0800

    add: session_10

---
+++ week_04/session_10/ex01.py
@@ -0,0 +1,100 @@
+# single_quotes ='This is single quotation text'
+# double_quotes="This is double quotation text"
+# multi_line_string= """
+# Multi Line String text
+# """
+
+# special_chars ="New Line: \n\tTabultor"
+# print(special_chars) 
+
+# text="Mongol"
+# print(text[0])
+# print(text[-1])
+
+# print(text[0:3])
+# print(text[:3])
+# print(text[3:])
+# print(text[::-1])
+
+# text= "Mongol uls"
+# print(len(text))
+
+# print(text.upper())
+# print(text.lower())
+# print(text.capitalize())
+# print(text.title())
+
+# print(text.find("gol"))
+# print(text.find("oros"))
+# print("gol" in text)
+# print(text.count("o"))
+
+# print(text.replace("uls","hel"))
+# words=text.split(" ")
+# print(words)
+
+# joined =",".join(words)
+# print(joined)
+
+# print(text.startswith("Mon"))
+# print(text.endswith("uls"))
+
+
+# padded=" Mongol "
+# print(padded.strip())
+# print(padded.lstrip())
+# print(padded.rstrip())
+
+# print("123".isdigit())
+# print("abc".isalpha())
+# print("abc123".isalnum())
+# print(" ".isspace())
+
+
+# name= "Bat"
+# age= 25
+# print("Sain uu, %s! Tanii nas %d."%(name, age))
+
+# print("Sain uu {}! Tanii nas{}.".format(name,age))
+# print("Sain uu {0}! Tanii nas{1}.".format(name,age))
+# print("Sain uu {name}! Tanii nas{age}.".format(name=name,age=age))
+
+
+# print(f"Sain uu {name}! Tanii nas{age}.")
+# print(f" Tanii 5jiliin daraah nas :{age+5}")
+
+# pi=3.14159
+# print(f"Pi too: {pi:.2f}")
+# print(f"Xuvi: {0.25:1%}")
+
+# first="Mongol"
+# last="Uls"
+# full =first+" "+last
+# print(full)
+
+# message = "Sain uu"
+# message +="Bat!"
+# print(message)
+
+
+# words=["Python", "hel", "surah", "xylbar"]
+# sentence = " ".join(words)
+# print(sentence)
+
+# text= "Hello World!"
+# vowels= [char for char in text if char.lower()in "aeiou"]
+# print(vowels)
+
+# uppercase=[char.upper() for char in text]
+# print(uppercase)
+# def print_numbers(first, second, third):
+#     print(first)
+#     print(second)
+#     print(third)
+x = 10
+
+def add_nums():
+    y = 2
+    z = x + y
+    print z
+
