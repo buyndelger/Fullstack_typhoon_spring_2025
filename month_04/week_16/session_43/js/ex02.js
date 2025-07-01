a9d17c0976f990995d1ac2bbbbbf89904ac2276c
Buyndelger <buyndelger80400727@gmail.com>
2025-07-01 17:51:01 +0800

    add: ss

--- month_04/week_16/session_43/js/ex02.js
+++ month_04/week_16/session_43/js/ex02.js
@@ -1,5 +1,4 @@
 let changeButton =document.getElementById('user-info');
-let welcome = document.getElementById('welcome')
 changeButton.addEventListener('click', function () {
     let existingUserName= prompt('Give me your name');
     let  name = prompt('What is your name');
@@ -21,9 +20,24 @@ changeButton.addEventListener('click', function () {
         alert(`User information saved!`)
     }
  
+    if(localStorage.getItem(existingUserName)){
+        let user = JSON.parse(localStorage.getItem(existingUserName));
+        const welcome= document.getElementById('welcome')
+        welcome.innerHTML+="h2>welcome back" +user.name+ "</h2>";
+        welcome.innerHTML+="<p2> Your Height is :" +user.height+"</p>"
+        welcome.innerHTML+="<p> Your favorite Animal is :" +user ['favorite-animal']+ "</p>" 
+        }else{
+            let nameOfUser = prompt('what is your name?')
+            let heightOfuser = prompt('What is your height')
+            let userFavoriteAnimal = prompt('What is your favourite animal?');
+    
+            let person={
+                name : nameOfUser,
+                height: heightOfuser,
+                'favorite-animal': userFavoriteAnimal
+            }
+    
+            localStorage.setItem(existingUserName, JSON.stringify(person))
+        }
 });
 
-for (let i =0; i<localStorage.length; i++){
-        welcome.innerHTML +=welcome[i];
-     
-    }
